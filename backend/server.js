e2907fa9a070882e1f92d8881829605814ed9acd
altinsyla <altinsyla11@gmail.com>
2024-04-02 13:22:04 +0200

    Commit #2

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,34 @@
 // It is used to import Express, Mongo, Cors
-// nodemon server.js to run server.jsc
+// nodemon server.js to run server.jscc
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
-
-const app = express();
 const port = process.env.PORT || 5001;
+const app = express();
 
+//connect to db
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://altinsyla997:N9xUmldDKypVWnW1@webprogramming.wluex0n.mongodb.net/?retryWrites=true&w=majority&appName=WebProgramming"
+mongoose.connect(uri)
+.then(() => 
+        console.log('Connected to MongoDB'))
+.catch((error) => 
+    console.log("Couldn't connect to MongoDB", error))
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    gender: String
+});
+
+const User = mongoose.model('User', UserSchema);
+app.get('/api/test', async(req, res) => {
+    //Krijimi i userit te ri
+    const newUser = new User({
+        name: 'new',
+        gender: 'm'
+    })
+    const savedUser = await newUser.save();
+    res.json(savedUser);
+});
 
 //this send Users data's to the local host 5001
 app.use(cors());
@@ -22,7 +44,7 @@ app.get('/api/users', async(req, res) => {
         gender: 'f'
         }
 ];
-    res.json(users);
+    res.json(savedUser);
 });
 
 // Listen for requests
